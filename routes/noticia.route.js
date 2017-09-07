329fac655addcbb4ac2e5d9c8b7d4db781baaed4
Byron Rosas <byron_mma@hotmail.es>
2017-09-07 18:41:28 -0500

    Cambios  API Noticia

--- routes/noticia.route.js
+++ routes/noticia.route.js
@@ -11,7 +11,7 @@ var carpeta_destino_imagen = multipart({uploadDir : './public/images/noticias'})
 //api.get('/album/:id', md_auth.ensureAuth, AlbumController.getAlbum);
 api.post('/save', [md_auth.ensureAuth,carpeta_destino_imagen], NoticiaController.saveNoticia);
 api.get('/lista/:page?', NoticiaController.getNoticias);
-api.put('/update/:id', md_auth.ensureAuth,NoticiaController.updateNoticia);
+api.put('/update/:id', [md_auth.ensureAuth,carpeta_destino_imagen],NoticiaController.updateNoticia);
 api.delete('/delete/:id', md_auth.ensureAuth, NoticiaController.deleteNoticia);
 // api.post('/upload-image-noticia/:id', [md_auth.ensureAuth, md_upload], AlbumController.uploadImage);
 api.get('/get-image-noticia/:imageFile', NoticiaController.getImageFile);
