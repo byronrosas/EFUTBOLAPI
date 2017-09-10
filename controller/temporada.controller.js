77eb2e2ff0cbb8a72a30ad8ca5f3716439639e9b
OscarToaQuiza <oscartoaquiza388@gmail.com>
2017-09-09 19:42:18 -0500

    Temporada Actualizada

--- controller/temporada.controller.js
+++ controller/temporada.controller.js
@@ -1,5 +1,6 @@
 'use strict'
-
+var path = require('path');
+var fs = require('fs');
 var Temporada = require('../models/temporada.model');
 
 //medtodo para subir imagenes
@@ -12,10 +13,10 @@ function saveTemporada(req,res){
     temporada.nombre_temporada = params.nombre_temporada;
     temporada.fecha_inicio = params.fecha_inicio;
     temporada.fecha_fin = params.fecha_fin;
-    temporada.id_usuario =         params.id_usuario;
+    temporada.id_usuario = params.id_usuario;
    
-    if (req.files) {
-
+    if (req.files && req.files.url_reglamento_temporada != undefined) {
+                console.log("Con Imagen");
                 var file_path = req.files.url_reglamento_temporada.path;
                 var file_split = file_path.split('\\');
                 var file_name = file_split[3];
@@ -46,7 +47,8 @@ function saveTemporada(req,res){
                     });
                 }
             } else {
-                 temporada.url_reglamento_temporada = "null";
+                console.log("Sin Imagen");
+                 //temporada.url_reglamento_temporada = "null";
                  temporada.save((err,temporadaGuardada) => {
                     if(err){
                         res.status(500).send({mensaje: "Error en el servidor"});
@@ -73,31 +75,80 @@ function saveTemporada(req,res){
 function updateTemporada(req, res) {
     var temporadaId = req.params.id;
     var update = req.body;
-
-    Temporada.findByIdAndUpdate(temporadaId, update, function (err, temporadaActualizada) {
-        if (err) {
-            res.status(500).send({ mensaje: "Error del servidor" });
-        } else {
-            if (!temporadaActualizada) {
-                res.status(404).send({ mensaje: "Error no se puede actualizar la temporada seleccionada" });
-            } else {
-                res.status(200).send({ temporadaActualizada })
-            }
-        }
-    })
+    if (req.files && req.files.url_reglamento_temporada!=undefined) {
+				var file_path = req.files.url_reglamento_temporada.path;
+				var file_split = file_path.split('\\');
+				var file_name = file_split[3];
+				//console.log(file_split);
+				var ext_split = file_name.split('\.');
+				var file_ext = ext_split[1];
+				//console.log(ext_split);
+				if (file_ext == 'pdf') {										
+					//console.log(equipo)					
+					update.url_reglamento_temporada=file_name;
+					Temporada.findByIdAndUpdate(temporadaId, update, (err, temporadaActualizada) => {
+						if(err){
+							res.status(500).send({mensaje: 'Error en el servidor'});
+						}else{
+							if(!temporadaActualizada){
+								res.status(404).send({mensaje: 'No se ha actualizado la noticia'});
+							}else{
+								//console.log(noticiaUpdated);
+								//BORRA ARCHIVO ANTERIOR
+								if(temporadaActualizada.url_reglamento_temporada != 'REGLAMENTO.pdf'){
+									var path_file = './public/images/reglamento/'+temporadaActualizada.url_reglamento_temporada;									
+									fs.exists(path_file, function(exists){
+										if(exists){
+											//console.log(exists);
+											fs.unlink(path_file,(err)=>{
+												if(err){
+													console.log("El archivo no pudo ser eliminado");
+												}else{
+													console.log("Archivo eliminado...");
+												}											
+											});
+										}else{
+											console.log("No hay imagen.");
+										}
+									});
+								}								
+								res.status(200).send({mensaje: temporadaActualizada});
+							}
+						}
+					});	
+				} else {
+					res.status(200).send({
+						mensaje: "Extension del archivo no valido"
+					});
+				}
+			} else {
+				
+                Temporada.findByIdAndUpdate(temporadaId, update, function (err, temporadaActualizada) {
+                    if (err) {
+                        res.status(500).send({ mensaje: "Error del servidor" });
+                    } else {
+                        if (!temporadaActualizada) {
+                            res.status(404).send({ mensaje: "Error no se puede actualizar la temporada seleccionada" });
+                        } else {
+                            res.status(200).send({ temporadaActualizada })
+                        }
+                    }
+                });
+			}
 }
 
 function updateTemporadaEstado(req,res){
     var temporadaId = req.params.id;
     var update = req.body.estado_temporada;
-    Temporada.findByIdAndUpdate(temporadaId, update, function (err, temporadaActualizada) {
+    console.log(update);
+    Temporada.findByIdAndUpdate(temporadaId, {estado_temporada:update}, function (err, temporadaActualizada) {
         if (err) {
             res.status(500).send({ mensaje: "Error del servidor" });
         } else {
             if (!temporadaActualizada) {
                 res.status(404).send({ mensaje: "Error no se puede actualizar la temporada seleccionada" });
             } else {
-                res.status(200).send({ temporadaActualizada })
+                res.status(200).send({ temporada : temporadaActualizada })
             }
         }
     })
@@ -132,10 +183,24 @@ function getTemporadaById(req,res){
     });
 }
 
+function getPDFFile(req, res){
+	var PDFFile = req.params.PDFFile;
+	var path_file = './public/images/reglamento/'+PDFFile;
+	fs.exists(path_file, function(exists){
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'No existe PDF Guardado...'});
+		}
+	});
+}
+
 
 module.exports = {
     saveTemporada,
     getTemporadas,
     getTemporadaById,
-    updateTemporada
+    updateTemporada,
+    getPDFFile,
+    updateTemporadaEstado
 }
